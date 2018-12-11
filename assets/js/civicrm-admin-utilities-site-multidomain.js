810fd7162a08090a745888afb237071e720e9d5e
Christian Wach <needle@haystack.co.uk>
2018-12-11 14:42:41 +0000

    Add stub JS and CSS files

---
+++ assets/js/civicrm-admin-utilities-site-multidomain.js
@@ -0,0 +1,29 @@
+/**
+ * Javascript for CiviCRM Admin Utilities Site Domain page.
+ *
+ * Implements functionality on the plugin's Site Domain page.
+ *
+ * @package CiviCRM_Admin_Utilities
+ */
+
+
+
+/**
+ * Pass the jQuery shortcut in.
+ *
+ * @since 0.6.2
+ *
+ * @param {Object} $ The jQuery object.
+ */
+( function( $ ) {
+
+	/**
+	 * Act on document ready.
+	 *
+	 * @since 0.6.2
+	 */
+	$(document).ready( function() {
+
+	});
+
+} )( jQuery );
